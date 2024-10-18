cb04fa571aad0fba69b431b8f0da9d2b29d541ae
Minhinh <nhatminhtran34@gmail.com>
2024-10-18 16:07:29 +1100

    Implement all dependencies and activity Manifest

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,16 @@
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
 }
 
 android {
     namespace = "com.example.assignment3_keeptrack"
     compileSdk = 34
-
+    viewBinding {
+        enable = true
+    }
     defaultConfig {
         applicationId = "com.example.assignment3_keeptrack"
         minSdk = 33
@@ -43,6 +47,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.navigation.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
